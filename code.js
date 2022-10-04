1de95340d8ada8329c74582494a001f6676dab8a
James Miles <jimdgmiles@gmail.com>
2022-10-04 02:08:03 +0100

    Update code.js

--- code.js
+++ code.js
@@ -1,6 +1,6 @@
 const startdecks = 8; // starting decks in shoe
 
-const sims = 30000;
+const sims = 40000;
 
 var counts = []; // frequency of cards remaining by type (index 0 = Aces, 1 = 2s, ... 12 = Kings)
 
@@ -55,206 +55,107 @@ function randomelements(n) { // returns an array of n values from 0 to n-1, rand
   return endlist;
 }
 
-function whowinsverbose(a, b, c, d, e, f) { // tells you who wins (0 = tie 1 = punto 2 = banco) and how
+function playgame(a, b, c, d, e, f, v) { // returns game result (0 to 9 = tie 0 to 9, 10 = punto 11 = banco) and how (when v=1)
   var pun = (a + c) % 10;
   var ban = (b + d) % 10;
-  document.getElementById('drum').innerHTML += "Top of deck is: " + a + ", " + b + ", " + c + ", " + d + ", " + e + ", " + f + "<br>Punto: " + pun + " Banco: " + ban + "<br>";
-  if (pun == 9 || pun == 8) {
+  if (v) { document.getElementById('drum').innerHTML += "Top of deck is: " + a + ", " + b + ", " + c + ", " + d + ", " + e + ", " + f + "<br>Punto: " + pun + " Banco: " + ban + "<br>" }
+  if (pun == 9 || pun == 8 || ban == 9 || ban == 8) {
     if (pun == ban) {
-      document.getElementById('drum').innerHTML += "TIE<br>";
-      return 0;
+      if (v) { document.getElementById('drum').innerHTML += "TIE<br>" }
+      return pun;
     }
     else if (pun > ban) {
-      document.getElementById('drum').innerHTML += "PUNTO WINS<br>";
-      return 1;
+      if (v) { document.getElementById('drum').innerHTML += "PUNTO WINS<br>" }
+      return 10;
     }
     else {
-      document.getElementById('drum').innerHTML += "BANCO WINS<br>";
-      return 2;
+      if (v) { document.getElementById('drum').innerHTML += "BANCO WINS<br>" }
+      return 11;
     }
   }
-  else if (pun == 7 || pun == 6) {
-    if (pun < ban) {
-      document.getElementById('drum').innerHTML += "BANCO WINS<br>";
-      return 2;
-    }
-    else if (pun == 7 && ban == 7) {
-      document.getElementById('drum').innerHTML += "TIE<br>";
-      return 0;
+  else if (pun == 7) {
+    if (ban == 7) {
+      if (v) { document.getElementById('drum').innerHTML += "TIE<br>" }
+      return pun;
     }
     else {
-      ban = ban + e;
-      document.getElementById('drum').innerHTML += "Banco draws extra card: " + e + "<br>Punto: " + pun + " Banco: " + ban + "<br>";
+      ban = (ban + e) % 10;
+      if (v) { document.getElementById('drum').innerHTML += "Banco draws extra card: " + e + "<br>Punto: " + pun + " Banco: " + ban + "<br>" }
       if (pun == ban) {
-        document.getElementById('drum').innerHTML += "TIE<br>";
-        return 0;
+        if (v) { document.getElementById('drum').innerHTML += "TIE<br>" }
+        return pun;
       }
       else if (pun > ban) {
-        document.getElementById('drum').innerHTML += "PUNTO WINS<br>";
-        return 1;
+        if (v) { document.getElementById('drum').innerHTML += "PUNTO WINS<br>" }
+        return 10;
       }
       else {
-        document.getElementById('drum').innerHTML += "BANCO WINS<br>";
-        return 2;
+        if (v) { document.getElementById('drum').innerHTML += "BANCO WINS<br>" }
+        return 11;
+      }
+    }
+  }
+  else if (pun == 6) {
+    if (ban == 7) {
+      if (v) { document.getElementById('drum').innerHTML += "BANCO WINS<br>" }
+      return 11;
+    }
+    else {
+      ban = (ban + e) % 10;
+      if (v) { document.getElementById('drum').innerHTML += "Banco draws extra card: " + e + "<br>Punto: " + pun + " Banco: " + ban + "<br>" }
+      if (pun == ban) {
+        if (v) { document.getElementById('drum').innerHTML += "TIE<br>" }
+        return pun;
+      }
+      else if (pun > ban) {
+        if (v) { document.getElementById('drum').innerHTML += "PUNTO WINS<br>" }
+        return 10;
+      }
+      else {
+        if (v) { document.getElementById('drum').innerHTML += "BANCO WINS<br>" }
+        return 11;
       }
     }
   }
   else {
-    pun = pun + e;
-    document.getElementById('drum').innerHTML += "Punto draws extra card: " + e + "<br>Punto: " + pun + " Banco: " + ban + "<br>";
-    if (pun == 9 || pun == 8){
-      if (pun == ban) {
-        document.getElementById('drum').innerHTML += "TIE<br>";
-        return 0;
-      }
-      else if (pun > ban) {
-        document.getElementById('drum').innerHTML += "PUNTO WINS<br>";
-        return 1;
-      }
-      else {
-        document.getElementById('drum').innerHTML += "BANCO WINS<br>";
-        return 2;
-      }
+    pun = (pun + e) % 10;
+    if (v) { document.getElementById('drum').innerHTML += "Punto draws extra card: " + e + "<br>Punto: " + pun + " Banco: " + ban + "<br>" }
+    if (ban < 3) {
+      ban = (ban + f) % 10;
+      if (v) { document.getElementById('drum').innerHTML += "Banco draws extra card: " + f + "<br>Punto: " + pun + " Banco: " + ban + "<br>" }
     }
-    else if (pun == 6 || pun == 7) {
-      if (pun < ban) {
-        document.getElementById('drum').innerHTML += "BANCO WINS<br>";
-        return 2;
-      }
-      else if (pun == 7 && ban == 7) {
-        document.getElementById('drum').innerHTML += "TIE<br>";
-        return 0;
-      }
-      else {
-        ban = ban + f;
-        document.getElementById('drum').innerHTML += "Banco draws extra card: " + f + "<br>Punto: " + pun + " Banco: " + ban + "<br>";
-        if (pun == ban) {
-          document.getElementById('drum').innerHTML += "TIE<br>";
-          return 0;
-        }
-        else if (pun > ban) {
-          document.getElementById('drum').innerHTML += "PUNTO WINS<br>";
-          return 1;
-        }
-        else {
-          document.getElementById('drum').innerHTML += "BANCO WINS<br>";
-          return 2;
-        }
-      }
+    else if (ban == 3 && e != 8) {
+      ban = (ban + f) % 10;
+      if (v) { document.getElementById('drum').innerHTML += "Banco draws extra card: " + f + "<br>Punto: " + pun + " Banco: " + ban + "<br>" }
     }
-    else {
-      if (pun < ban) {
-        document.getElementById('drum').innerHTML += "BANCO WINS<br>";
-        return 2;
-      }
-      else {
-        ban = ban + f;
-        document.getElementById('drum').innerHTML += "Banco draws extra card: " + f + "<br>Punto: " + pun + " Banco: " + ban + "<br>";
-        if (pun == ban) {
-          document.getElementById('drum').innerHTML += "TIE<br>";
-          return 0;
-        }
-        else if (pun > ban) {
-          document.getElementById('drum').innerHTML += "PUNTO WINS<br>";
-          return 1;
-        }
-        else {
-          document.getElementById('drum').innerHTML += "BANCO WINS<br>";
-          return 2;
-        }
-      }
+    else if (ban == 4 && e != 1 && e != 8 && e != 9 && e != 0) {
+      ban = (ban + f) % 10;
+      if (v) { document.getElementById('drum').innerHTML += "Banco draws extra card: " + f + "<br>Punto: " + pun + " Banco: " + ban + "<br>" }
     }
-  }
-}
-
-function whowins(a, b, c, d, e, f) { // tells you who wins (0 = tie 1 = punto 2 = banco) and how
-  var pun = (a + c) % 10;
-  var ban = (b + d) % 10;
-  if (pun == 9 || pun == 8) {
+    else if (ban == 5 && e != 1 && e != 2 && e != 3 && e != 8 && e != 9 && e != 0) {
+      ban = (ban + f) % 10;
+      if (v) { document.getElementById('drum').innerHTML += "Banco draws extra card: " + f + "<br>Punto: " + pun + " Banco: " + ban + "<br>" }
+    }
+    else if (ban == 6 && e != 1 && e != 2 && e != 3 && e != 4 && e != 5 && e != 8 && e != 9 && e != 0) {
+      ban = (ban + f) % 10;
+      if (v) { document.getElementById('drum').innerHTML += "Banco draws extra card: " + f + "<br>Punto: " + pun + " Banco: " + ban + "<br>" }
+    }
     if (pun == ban) {
-      return 0;
+      if (v) { document.getElementById('drum').innerHTML += "TIE<br>" }
+      return pun;
     }
     else if (pun > ban) {
-      return 1;
+      if (v) { document.getElementById('drum').innerHTML += "PUNTO WINS<br>" }
+      return 10;
     }
     else {
-      return 2;
-    }
-  }
-  else if (pun == 7 || pun == 6) {
-    if (pun < ban) {
-      return 2;
-    }
-    else if (pun == 7 && ban == 7) {
-      return 0;
-    }
-    else {
-      ban = ban + e;
-      if (pun == ban) {
-        return 0;
-      }
-      else if (pun > ban) {
-        return 1;
-      }
-      else {
-        return 2;
-      }
-    }
-  }
-  else {
-    pun = pun + e;
-    if (pun == 9 || pun == 8){
-      if (pun == ban) {
-        return 0;
-      }
-      else if (pun > ban) {
-        return 1;
-      }
-      else {
-        return 2;
-      }
-    }
-    else if (pun == 6 || pun == 7) {
-      if (pun < ban) {
-        return 2;
-      }
-      else if (pun == 7 && ban == 7) {
-        return 0;
-      }
-      else {
-        ban = ban + f;
-        if (pun == ban) {
-          return 0;
-        }
-        else if (pun > ban) {
-          return 1;
-        }
-        else {
-          return 2;
-        }
-      }
-    }
-    else {
-      if (pun < ban) {
-        return 2;
-      }
-      else {
-        ban = ban + f;
-        if (pun == ban) {
-          return 0;
-        }
-        else if (pun > ban) {
-          return 1;
-        }
-        else {
-          return 2;
-        }
-      }
+      if (v) { document.getElementById('drum').innerHTML += "BANCO WINS<br>" }
+      return 11;
     }
   }
 }
 
-function simgame() { // simulates a game from the submitted deck information with explanation
+function simgame(v) { // simulates a game from the submitted deck information (v=1 gives explanation)
   var cards = []; // cards remaining recorded individually (in order they will be drawn)
 
   // builds 'cards' from frequency counts
@@ -279,54 +180,34 @@ function simgame() { // simulates a game from the submitted deck information wit
   }
   cards = shuffled;
 
-  return whowins(cards[0], cards[1], cards[2], cards[3], cards[4], cards[5]);
-
-}
-
-function simgameverbose() { // simulates a game from the submitted deck information with explanation
-  var cards = []; // cards remaining recorded individually (in order they will be drawn)
-
-  // builds 'cards' from frequency counts
-
-  for (var i = 0; i < 9; i++) {
-    for (var j = 0; j < counts[i]; j++) {
-      cards[cards.length] = i + 1;
-    }
+  if (v == 1) {
+    return playgame(cards[0], cards[1], cards[2], cards[3], cards[4], cards[5], 1);
   }
-  for (var i = 9; i < 13; i++) {
-    for (var j = 0; j < counts[i]; j++) {
-      cards[cards.length] = 0;
-    }
+  else {
+    return playgame(cards[0], cards[1], cards[2], cards[3], cards[4], cards[5]);
   }
-
-  // shuffles 'cards'
-
-  var shuffled = [];
-  var shuffle = randomelements(cards.length);
-  for (var i = 0; i < cards.length; i++) {
-    shuffled[i] = cards[shuffle[i]];
-  }
-  cards = shuffled;
-
-  return whowinsverbose(cards[0], cards[1], cards[2], cards[3], cards[4], cards[5]);
 
 }
 
 function launch() {
-  var wins = [];
-  for (var i=0; i<3; i++){
-    wins[i]=0;
+  var result = [];
+  for (var i = 0; i < 12; i++) {
+    result[i] = 0;
   }
   document.getElementById("bongo").innerHTML = '';
   document.getElementById("drum").innerHTML = '';
-  for(i=0; i<sims/2; i++){
-    wins[simgame()]++;
+  for (i = 0; i < sims / 2; i++) {
+    result[simgame()]++;
   }
-  wins[simgameverbose()]++;
-  for(i=0; i<sims/2; i++){
-    wins[simgame()]++;
+  result[simgame(1)]++;
+  for (i = 0; i < sims / 2; i++) {
+    result[simgame()]++;
   }
-  document.getElementById("bongo").innerHTML = "Tie: "+Math.round(100000*wins[0]/(sims+1))/1000+"% Punto: "+Math.round(100000*wins[1]/(sims+1))/1000+"% Banco: "+Math.round(100000*wins[2]/(sims+1))/1000+"%";
+  for (var i = 0; i < 10; i++) {
+    document.getElementById("bongo").innerHTML += "Tie-" + i + ": " + Math.round(100000 * result[i] / (sims + 1)) / 1000 + "%<br>";
+  }
+  document.getElementById("bongo").innerHTML += "Punto: " + Math.round(100000 * result[10] / (sims + 1)) / 1000 + "%<br>";
+  document.getElementById("bongo").innerHTML += "Banco: " + Math.round(100000 * result[11] / (sims + 1)) / 1000 + "%<br>";
 }
 
 updateui();
