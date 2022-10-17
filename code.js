9cf37bc61264c33f0b081bee7d14ba84e089c776
James Miles <jimdgmiles@gmail.com>
2022-10-17 01:27:33 +0100

    sesh-progress

--- code.js
+++ code.js
@@ -1,44 +1,85 @@
 const startdecks = 8; // starting decks in shoe
 
-const sims = 100000;
+const sims = 500000;
 
 var counts = []; // frequency of cards remaining by type (index 0 = Aces, 1 = 2s, ... 12 = Kings)
 
-for (var i = 0; i < 13; i++) { // generates initial counts from 'shoes'
+for (var i = 0; i < 9; i++) { // generates initial counts from 'shoes'
   counts[i] = 4 * startdecks;
 }
+counts[9] = 16 * startdecks;
+
+var odds = [];
+odds[odds.length] = 150;
+odds[odds.length] = 215;
+odds[odds.length] = 225;
+odds[odds.length] = 200;
+odds[odds.length] = 120;
+odds[odds.length] = 110;
+odds[odds.length] = 45;
+odds[odds.length] = 45;
+odds[odds.length] = 80;
+odds[odds.length] = 80;
 
 function alter(m, n) { // alters frequency of an individual card type
-  counts[m] += n;
+  counts[m] += -n;
   updateui();
 }
 
 function deck(n) { // adds or subtracts decks (= 4 of each card type)
-  for (var i = 0; i < 13; i++) {
-    counts[i] += 4 * n;
+  for (var i = 0; i < 9; i++) {
+    counts[i] += 4 * -n;
   }
+  counts[9] += 16 * -n;
   updateui();
 }
 
 function updateui() { // updates displayed card frequencies
-  document.getElementById('a-count').innerHTML = counts[0];
-  document.getElementById('2-count').innerHTML = counts[1];
-  document.getElementById('3-count').innerHTML = counts[2];
-  document.getElementById('4-count').innerHTML = counts[3];
-  document.getElementById('5-count').innerHTML = counts[4];
-  document.getElementById('6-count').innerHTML = counts[5];
-  document.getElementById('7-count').innerHTML = counts[6];
-  document.getElementById('8-count').innerHTML = counts[7];
-  document.getElementById('9-count').innerHTML = counts[8];
-  document.getElementById('t-count').innerHTML = counts[9];
-  document.getElementById('j-count').innerHTML = counts[10];
-  document.getElementById('q-count').innerHTML = counts[11];
-  document.getElementById('k-count').innerHTML = counts[12];
+  var cardsgone = [];
+  cardsgone[0] = 16 * startdecks - counts[9];
+  for (var i = 1; i < 10; i++) {
+    cardsgone[i] = 4 * startdecks - counts[i - 1];
+  }
+  document.getElementById('1-count').innerHTML = cardsgone[1];
+  document.getElementById('2-count').innerHTML = cardsgone[2];
+  document.getElementById('3-count').innerHTML = cardsgone[3];
+  document.getElementById('4-count').innerHTML = cardsgone[4];
+  document.getElementById('5-count').innerHTML = cardsgone[5];
+  document.getElementById('6-count').innerHTML = cardsgone[6];
+  document.getElementById('7-count').innerHTML = cardsgone[7];
+  document.getElementById('8-count').innerHTML = cardsgone[8];
+  document.getElementById('9-count').innerHTML = cardsgone[9];
+  document.getElementById('0-count').innerHTML = cardsgone[0];
   var total = 0;
-  for (var i = 0; i < 13; i++) {
+  for (var i = 0; i < 10; i++) {
     total += counts[i];
   }
   document.getElementById('total').innerHTML = total;
+
+  var systemcount = [];
+  systemcount[systemcount.length] = [-3, 2, 2, 2, 2, 1, 1, 0, 1, 1];
+  systemcount[systemcount.length] = [-1, -6, 2, 2, 2, 2, 1, 1, 0, 0];
+  systemcount[systemcount.length] = [-1, -1, -6, 2, 2, 2, 2, 1, 2, 0];
+  systemcount[systemcount.length] = [-1, -1, -1, -6, 2, 2, 3, 3, 0, 2];
+  systemcount[systemcount.length] = [0, -1, 0, 0, -6, 1, 2, 2, 2, 0];
+  systemcount[systemcount.length] = [0, 0, -1, -1, 0, -6, 2, 2, 2, 2];
+  systemcount[systemcount.length] = [1, 0, 0, 0, 0, 0, -7, 1, 1, 1];
+  systemcount[systemcount.length] = [1, 1, 0, 0, 0, 0, 0, -8, 2, 1];
+  systemcount[systemcount.length] = [1, 1, 1, 0, 0, 0, 0, 0, -7, 1];
+  systemcount[systemcount.length] = [1, 1, 1, 1, 0, 0, 0, 0, 1, -8];
+  triggers = [7,7,6,7,7,7,7,4,6,6];
+
+  for (var i = 0; i < 10; i++) {
+    var countnumber = Math.round(100 * (systemcount[i][1] * cardsgone[1] + systemcount[i][2] * cardsgone[2] + systemcount[i][3] * cardsgone[3] + systemcount[i][4] * cardsgone[4] + systemcount[i][5] * cardsgone[5] + systemcount[i][6] * cardsgone[6] + systemcount[i][7] * cardsgone[7] + systemcount[i][8] * cardsgone[8] + systemcount[i][9] * cardsgone[9] + systemcount[i][0] * cardsgone[0]) / (total / 52)) / 100;
+    document.getElementById('' + i + '-tcount').innerHTML = countnumber + " (" + triggers[i] + ")";
+    if(countnumber>triggers[i]){
+      document.getElementById('' + i + '-tcount').style.color = "yellow";
+    }
+    else{
+      document.getElementById('' + i + '-tcount').style.color = "rgb(130, 130, 0)";
+    }
+  }
+
 }
 
 function randomelements(m, n) { // returns an array of m randomly-chosen values from 0 to n-1
@@ -144,7 +185,7 @@ function playgame(a, b, c, d, e, f, v) { // returns game result (0 to 9 = tie 0 
 }
 
 function simgame(v) { // simulates a game from the submitted deck information (v=1 gives explanation)
-  var cards = []; // cards remaining recorded individually (in order they will be drawn)
+  var cards = []; // cards remaining recorded individually
 
   // builds 'cards' from frequency counts
 
@@ -153,10 +194,8 @@ function simgame(v) { // simulates a game from the submitted deck information (v
       cards[cards.length] = i + 1;
     }
   }
-  for (var i = 9; i < 13; i++) {
-    for (var j = 0; j < counts[i]; j++) {
-      cards[cards.length] = 0;
-    }
+  for (var j = 0; j < counts[9]; j++) {
+    cards[cards.length] = 0;
   }
 
   // picks 6 random cards
@@ -177,6 +216,10 @@ function simgame(v) { // simulates a game from the submitted deck information (v
 }
 
 function launch() {
+  var edge = [];
+  for (var i = 0; i < 10; i++) {
+    edge[i] = 0;
+  }
   var result = [];
   for (var i = 0; i < 12; i++) {
     result[i] = 0;
@@ -195,6 +238,20 @@ function launch() {
   }
   document.getElementById("bongo").innerHTML += "Punto: " + Math.round(100000 * result[10] / (sims + 1)) / 1000 + "%<br>";
   document.getElementById("bongo").innerHTML += "Banco: " + Math.round(100000 * result[11] / (sims + 1)) / 1000 + "%<br>";
+
+  var ev = 0;
+
+  for (i = 0; i < 10; i++) {
+
+    ev = Math.floor(10000 * ((odds[i] + 1) * result[i] / (sims + 1) - 1)) / 100;
+
+    if (ev < 0) {
+      document.getElementById("" + i + "-edge").innerHTML = "<span style='color:red'>" + ev + "%</span>";
+    }
+    else {
+      document.getElementById("" + i + "-edge").innerHTML = "<span style='color:green'>" + ev + "%</span>";
+    }
+  }
 }
 
 updateui();
